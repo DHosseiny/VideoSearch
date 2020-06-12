71a0abf9a91ab55f8aceb9f96e6177036049810d
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-12 12:41:01 +0430

    Add VideoRemoteDataSource and API data conversion function

--- app/src/main/java/david/hosseini/videosearch/api/VideoRemoteDataSource.kt
+++ app/src/main/java/david/hosseini/videosearch/api/VideoRemoteDataSource.kt
@@ -1,7 +1,24 @@
 package david.hosseini.videosearch.api
 
+import david.hosseini.videosearch.api.model.Video
 import retrofit2.http.GET
 import retrofit2.http.Query
+import javax.inject.Inject
+
+
+class VideoRemoteDataSource @Inject constructor(private val vimeoApi: VimeoApi) {
+
+    suspend fun searchVideos(keyword: String): List<Video> =
+        vimeoApi.searchVideos(keyword).toVideos()
+
+    private fun SearchResponse.toVideos(): List<Video> = data.map {
+        //Here considered uri is always in this format : /videos/{id}
+        val id = it.uri.substringAfterLast('/').toInt()
+        //For brevity considered there is always a picture
+        val pictureLink = it.pictures.sizes[0].link
+        Video(id, it.name, pictureLink)
+    }
+}
 
 interface VimeoApi {
 
---
+++ app/src/main/java/david/hosseini/videosearch/api/model/Video.kt
@@ -0,0 +1,3 @@
+package david.hosseini.videosearch.api.model
+
+data class Video(val id: Int, val name: String, val pictureLink: String)
