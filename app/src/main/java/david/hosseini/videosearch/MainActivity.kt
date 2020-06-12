57b4ea8751780266c1811f66d0a0b43e0249332f
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-12 12:18:32 +0430

    Add MainActivity and show ListFragment on it

---
+++ app/src/main/java/david/hosseini/videosearch/MainActivity.kt
@@ -0,0 +1,5 @@
+package david.hosseini.videosearch
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(R.layout.activity_list)
---
+++ app/src/main/java/david/hosseini/videosearch/list/ListFragment.kt
@@ -0,0 +1,6 @@
+package david.hosseini.videosearch.list
+
+import androidx.fragment.app.Fragment
+import david.hosseini.videosearch.R
+
+class ListFragment : Fragment(R.layout.fragment_list)
