b934933fc437cc328e0f8286e1bc1a6930229213
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-12 16:47:44 +0430

    Add VideosRepository

---
+++ app/src/main/java/david/hosseini/videosearch/list/VideosRepository.kt
@@ -0,0 +1,20 @@
+package david.hosseini.videosearch.list
+
+import david.hosseini.videosearch.api.VideoRemoteDataSource
+import david.hosseini.videosearch.api.model.Video
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class VideosRepository @Inject constructor(private val remoteDataSource: VideoRemoteDataSource,
+                                           private val dispatcher: CoroutineDispatcher) {
+
+
+    suspend fun searchVideos(keyword: String): List<Video> {
+        return withContext(dispatcher) {
+            runCatching {
+                remoteDataSource.searchVideos(keyword)
+            }.getOrDefault(emptyList())
+        }
+    }
+}
