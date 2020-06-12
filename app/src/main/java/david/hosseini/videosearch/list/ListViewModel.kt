25949254fa273081288e73b396e25ea2814e3894
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-12 17:10:14 +0430

    Add searchKeyword function to search and videos to observe on search result in ListViewModel

--- app/src/main/java/david/hosseini/videosearch/list/ListViewModel.kt
+++ app/src/main/java/david/hosseini/videosearch/list/ListViewModel.kt
@@ -1,11 +1,27 @@
 package david.hosseini.videosearch.list
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import david.hosseini.videosearch.api.model.Video
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
-class ListViewModel(private val repository: VideosRepository) : ViewModel()
+class ListViewModel(private val repository: VideosRepository) : ViewModel() {
+
+
+    private val _videos = MutableLiveData<List<Video>>()
+    val videos: LiveData<List<Video>> = _videos
+
+    fun searchKeyword(keyword: String) {
+
+        if (keyword.length < 3) return //don't do search when user is typed few characters
+
+        viewModelScope.launch {
+            val videos = repository.searchVideos(keyword)
+            _videos.value = videos
+        }
+    }
+}
 
 @Singleton
 class ListViewModelFactory @Inject constructor(private val repository: VideosRepository) :
