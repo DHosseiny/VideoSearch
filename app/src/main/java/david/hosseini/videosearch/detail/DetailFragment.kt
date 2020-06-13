002c1180d8eee4344876c305e95fd62a9f7f15ae
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-13 16:01:59 +0430

    Show video details

--- app/src/main/java/david/hosseini/videosearch/detail/DetailFragment.kt
+++ app/src/main/java/david/hosseini/videosearch/detail/DetailFragment.kt
@@ -1,10 +1,14 @@
 package david.hosseini.videosearch.detail
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import david.hosseini.videosearch.R
 import david.hosseini.videosearch.api.model.Video
+import kotlinx.android.synthetic.main.fragment_detail.*
 
-class DetailFragment : Fragment() {
+class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     companion object {
         fun newInstance(video: Video): DetailFragment {
@@ -19,5 +23,22 @@ class DetailFragment : Fragment() {
         }
 
         private const val ARG_VIDEO = "id"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        showVideoDetails()
+    }
+
+    private fun showVideoDetails() {
+        val video = requireArguments().getParcelable<Video>(ARG_VIDEO)!!
+
+        Glide.with(this)
+            .load(video.pictureLink)
+            .into(imageVideo)
+
+        textTitle.text = video.name
+        textDescription.text = video.description
+        textSender.text = getString(R.string.sender, video.userName)
     }
 }
