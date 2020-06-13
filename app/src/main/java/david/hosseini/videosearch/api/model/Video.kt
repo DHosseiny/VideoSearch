cebcb5c738884e94adf5428f08370b052598d4b3
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-13 15:20:26 +0430

    Set Video class parcelable

--- app/src/main/java/david/hosseini/videosearch/api/model/Video.kt
+++ app/src/main/java/david/hosseini/videosearch/api/model/Video.kt
@@ -1,7 +1,11 @@
 package david.hosseini.videosearch.api.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Video(val id: Int,
                  val name: String,
                  val description: String,
                  val userName: String,
-                 val pictureLink: String)
+                 val pictureLink: String) : Parcelable
