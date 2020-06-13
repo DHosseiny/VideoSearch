bd9829b56a28731a2ca68bbf2af785cdc8bfc710
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-13 14:45:03 +0430

    Fix ListFragment is visible under second fragment when added

--- app/src/main/java/david/hosseini/videosearch/MainActivity.kt
+++ app/src/main/java/david/hosseini/videosearch/MainActivity.kt
@@ -1,5 +1,20 @@
 package david.hosseini.videosearch
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import david.hosseini.videosearch.list.ListFragment
+import kotlinx.android.synthetic.main.activity_list.*
 
-class MainActivity : AppCompatActivity(R.layout.activity_list)
+class MainActivity : AppCompatActivity(R.layout.activity_list) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (fragmentList == null) {
+            supportFragmentManager.commit {
+                add(R.id.frameContainer, ListFragment())
+            }
+        }
+    }
+}
